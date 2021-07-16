b9b37d8aea52477c00e8ad40c791da692bdfc8ec
rzlams <lamsarmiento@gmail.com>
2021-07-16 17:32:14 -0400

    active plan config

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 'use strict'
+
+const activeClass = 'active'
+const config = {
+  activePlan: 'pro', // 'mvp' | 'basic' | 'pro'
+}
+
+const cardItems = document.querySelectorAll('.card')
+
+Array.from(cardItems).forEach(function (card) {
+  const isActivePlan = card.classList.contains(config.activePlan)
+
+  if (isActivePlan) card.classList.add(activeClass)
+})
